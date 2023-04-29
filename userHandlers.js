d748beba989cdf02cdf0ee628b15b56b94116963
Chl0l <chloe.kim.elle@gmail.com>
2023-04-29 14:36:04 +0200

    Express 02 - Lecture depuis une base de données exercice

---
+++ userHandlers.js
@@ -0,0 +1,42 @@
+const database = require("./database");
+
+// Créer une route GET /api/users, cette route doit renvoyer un statut 200 et une liste d'utilisateurs de la base de données au format json
+
+const getUsers = (req, res) => {
+    database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(200)
+    })
+  }
+
+// Créez une route GET /api/users/:id qui renverra uniquement l'utilisateur de la base de données correspondant à l'identifiant défini dans l'url
+// S'il y a un utilisateur qui correspond aux paramètres, renvoie une réponse avec un statut 200 et l'utilisateur correspondant en tant qu'objet json
+// Sinon, retourne un statut 404 avec un message "Not Found"
+
+const getUserById = (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    database
+      .query("select * from users where id = ?", [id])
+      .then(([users]) => {
+        if (users[0] != null) {
+          res.json(users[0]);
+        } else {
+          res.status(404).send("Not Found");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
+  };
+  
+  module.exports = {
+    getUsers,
+    getUserById,
+  };
