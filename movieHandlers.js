476f81f724160d7b94d452b57449bce1cfe2ddeb
Chl0l <chloe.kim.elle@gmail.com>
2023-05-09 17:21:57 +0200

    Express 03

--- movieHandlers.js
+++ movieHandlers.js
@@ -57,7 +57,24 @@ const getMovieById = (req, res) => {
     });
 };
 
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query("insert into movies (title, director, year, color, duration) values (?, ?, ?, ?, ?)",
+    [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.location(`/api/movies/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the movie");
+    });
+};
+
 module.exports = {
   getMovies,
   getMovieById,
+  postMovie,
 };
