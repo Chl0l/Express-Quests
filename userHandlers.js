66ed8094ff2a7a6ad48ddc5b08725a54ff9b74ff
Chl0l <chloe.kim.elle@gmail.com>
2023-05-09 18:34:27 +0200

    Express 04

--- userHandlers.js
+++ userHandlers.js
@@ -36,6 +36,8 @@ const getUserById = (req, res) => {
       });
   };
 
+// Crée une route POST pour /api/users qui insérera un nouvel utilisateur dans la base de données
+
 const postUser = (req, res) => {
   const { firstname, lastname, email, city, language } = req.body;
   database
@@ -49,9 +51,31 @@ const postUser = (req, res) => {
       res.status(500).send("Error saving the user");
     });
 };
-  
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?", [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the user");
+    });
+};
+
   module.exports = {
     getUsers,
     getUserById,
     postUser,
+    updateUser,
   };
